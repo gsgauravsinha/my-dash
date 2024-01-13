45f8bbf0428f7261ddd59d6bc4849259ed5ba3b1
gsgauravsinha <gouravsinhaiitpatna@gmail.com>
2024-01-13 19:58:05 +0530

    add requirements.txt

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ if plot:
     if selected_state == 'Overall India':
         fig = px.scatter_mapbox(df,lat = 'Latitude', lon = 'Longitude', zoom = 4, size_max=30,
                                 size = selected_primary, color= selected_secondary, mapbox_style='carto-positron',
-                                width = 1200, height = 700 #, color_continuous_scale=[[0, 'red'], [0.5, 'rgb(0, 0, 255)'],[1, 'green']]
+                                width = 1200, height = 700 , color_continuous_scale=[[0, 'red'], [0.5, 'rgb(0, 0, 255)'],[1, 'green']]
                                 , hover_name='District')
         st.plotly_chart(fig, use_container_width=True)
     else:
@@ -31,6 +31,6 @@ if plot:
         fig = px.scatter_mapbox(state_df, lat='Latitude', lon='Longitude', zoom=6, size_max=25,
                                 size=selected_primary, color=selected_secondary, mapbox_style='carto-positron',
                                 width=1200, height=700
-                                #,color_continuous_scale=[[0, 'red'], [0.5, 'rgb(0, 0, 255)'], [1, 'green']]
+                                ,color_continuous_scale=[[0, 'red'], [0.5, 'rgb(0, 0, 255)'], [1, 'green']]
                                 ,hover_name='District')
         st.plotly_chart(fig, use_container_width=True)
